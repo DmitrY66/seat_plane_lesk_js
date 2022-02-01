4452649fb45a96477bbb6d4ad332863f758b2722
Dmitri Gorun <iii448@mail.ru>
2022-02-01 18:20:39 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+import start from "./modules/start.js";
+import getFormPerson from "./modules/formPerson.js";
+
+const init = (selecorApp, title) => {
+
+  const app = document.querySelector(selecorApp);
+
+  const { main, firstForm } = start(app, title);
+
+  firstForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const forms = getFormPerson(firstForm.count.value);
+    firstForm.remove();
+
+    main.append(...forms)
+
+  });
+
+
+
+};
+
+init('.app', 'Выберите тур');
+
+
+
+
+
